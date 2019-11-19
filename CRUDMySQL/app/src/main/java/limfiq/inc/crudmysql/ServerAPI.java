25aeaa462582875d441efb4c6c96177d1d69815e
ivan <ivansandria4@gmail.com>
2019-11-19 12:20:28 +0700

    UTS

---
+++ CRUDMySQL/app/src/main/java/limfiq/inc/crudmysql/ServerAPI.java
@@ -0,0 +1,8 @@
+package limfiq.inc.crudmysql;
+
+public class ServerAPI {
+    public static final String URL_DATA = "http://192.168.43.48/crud/view_data.php";
+    public static final String URL_INSERT = "http://192.168.43.48/crud/create_data.php";
+    public static final String URL_DELETE = "http://192.168.43.48/crud/delete_data.php";
+    public static final String URL_UPDATE = "http://192.168.43.48/crud/update_data.php";
+}
